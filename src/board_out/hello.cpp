54f67c77a8ec31d44013d3c7326886d734846070
huangrichao <huangrichao@growingio.com>
2019-06-23 19:01:36 +0800

    补全目录

---
+++ src/board_out/hello.cpp
@@ -0,0 +1,19 @@
+/*
+ * @Author: HuangRichao 
+ * @Date: 2019-06-23 09:02:31 
+ * @Last Modified by:   HuangRichao 
+ * @Last Modified time: 2019-06-23 09:02:31 
+ */
+
+
+#include <iostream>
+using namespace std;
+int main(int args,char* argv[]){
+    int x(1024);
+    int y;
+    cout<<"请输入一个整数"<<endl;
+    cin>>y;
+    std::cout<<"Hello !:"<<x<<y<<std::endl;
+    return 0;
+}
+
